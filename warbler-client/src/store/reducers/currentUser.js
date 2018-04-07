6d88f71fbe65eb755d342ca2690d51e001db720d
Haichao Wang <wanghaichao2195@gmail.com>
2018-04-07 15:55:24 -0400

    redirecting with react router and redux

---
+++ warbler-client/src/store/reducers/currentUser.js
@@ -0,0 +1,21 @@
+import {SET_CURRENT_USER} from "../actionTypes";
+
+const DEFAULT_STATE = {
+    isAuthenticated: false,// hopefully be true, when logged in
+    user:{} // all the user info when logged in
+};
+
+export default (state = DEFAULT_STATE, action) => {
+    switch(action.type){
+        case SET_CURRENT_USER:
+            return {
+                // turn empty object into false or if there are keys, true
+                isAuthenticated: !!Object.keys(action.user).length,
+                //equal to isAuthenticated: !!Object.keys(action.user).length > 0
+                //equal to use Boolean()
+                user: action.user
+            };
+        default:
+            return state;
+    }
+};
